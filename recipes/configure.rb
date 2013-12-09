99d9127e7ab1f6e1eef2bb3641ee5cbfe1016405
Brian Schroeder <bts@gmail.com>
2013-12-09 15:38:53 -0800

    Create log,data dirs post-install
    This change allows for log and data dirs to live in the kafka install
    dir if we wish. If we create them before installation, `mv`ing the kafka
    dir would put it inside the existing dir instead of renaming it to live
    at the installation target.

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -3,11 +3,16 @@
 # Recipe:: configure
 #
 
-config_dir = "#{node[:kafka][:install_dir]}/config"
 user = node[:kafka][:user]
 group = node[:kafka][:group]
 
+config_dir = "#{node[:kafka][:install_dir]}/config"
+log_dir = node[:kafka][:log_dir]
+data_dir = node[:kafka][:data_dir]
+
 directory config_dir do
+  not_if { File.directory?(config_dir) }
+
   owner user
   group group
   mode '755'
@@ -31,4 +36,24 @@ template "#{config_dir}/#{node[:kafka][:config]}" do
   owner user
   group group
   mode  '644'
+end
+
+directory log_dir do
+  not_if { File.directory?(log_dir) }
+
+  owner   user
+  group   group
+  mode    '755'
+  recursive true
+  action :create
+end
+
+directory data_dir do
+  not_if { File.directory?(data_dir) }
+
+  owner   user
+  group   group
+  mode    '755'
+  recursive true
+  action :create
 end
