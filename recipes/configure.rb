b044476e5e2e114eed7201eba0cedd1ce78a9754
Brian Schroeder <bts@gmail.com>
2013-12-09 15:28:51 -0800

    Ensure config dir exists

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -4,11 +4,21 @@
 #
 
 config_dir = "#{node[:kafka][:install_dir]}/config"
+user = node[:kafka][:user]
+group = node[:kafka][:group]
+
+directory config_dir do
+  owner user
+  group group
+  mode '755'
+  recursive true
+  action :create
+end
 
 template "#{config_dir}/#{node[:kafka][:log4j_config]}" do
   source  "log4j.properties.erb"
-  owner node[:kafka][:user]
-  group node[:kafka][:group]
+  owner user
+  group group
   mode  '644'
   variables({
     :process => 'kafka',
@@ -18,7 +28,7 @@ end
 
 template "#{config_dir}/#{node[:kafka][:config]}" do
   source  "server.properties.erb"
-  owner node[:kafka][:user]
-  group node[:kafka][:group]
+  owner user
+  group group
   mode  '644'
 end
