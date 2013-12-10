31b0d0daee8e6cd40880c637783a173f7281a897
Brian Schroeder <bts@gmail.com>
2013-12-10 15:59:59 -0800

    Upgrade Java

--- metadata.rb
+++ metadata.rb
@@ -8,7 +8,7 @@ description      'Installs and runs Kafka 0.8 on CentOS'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.4'
 
-depends 'java', '~> 1.11.6'
+depends 'java', '~> 1.13.0'
 # ZK is only needed for development, but its attributes aren't pulled in
 # correctly if this isn't listed here:
 depends 'zookeeper', '~> 1.4.6'
